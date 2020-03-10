05c35e3a18249170679abf292a221ef6d459c717
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-10 23:22:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if @sender.balance - @sender.amount
+      "rejected"
+    else
+      "successful"
+    end
+  end
 end
