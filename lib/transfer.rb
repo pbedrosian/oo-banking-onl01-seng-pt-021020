9dec83b6e6ef7bba5b4172cb34fca580636e1c12
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-10 23:46:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
   end
 
   def reverse_transfer
-    @sender.balance -= @amount
-    @receiver.balance += @amount
+    @sender.balance += @amount
+    @receiver.balance -= @amount
   end
 end
