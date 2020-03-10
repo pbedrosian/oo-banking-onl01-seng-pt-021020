f6cf751d2734d4ed4cd89f1110b405b64c938d64
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-10 23:39:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,6 +14,13 @@ class Transfer
   end
 
   def execute_transaction
-    @sender.balance += @receiver.balance
+        if @sender.balance > @amount && @status == "pending" && valid?
+          @sender.balance -= @amount
+          @receiver.balance += @amount
+          @status = "complete"
+        else
+          @status = "rejected"
+          return "Transaction rejected. Please check your account balance."
+        end
   end
 end
